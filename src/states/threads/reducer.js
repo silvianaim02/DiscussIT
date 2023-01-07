39ef0163bc82fdfcd54a094799337d7092b03c84
silvianaim02 <silvianaim68@gmail.com>
2023-01-07 18:58:23 +0700

    make test error threadReducer

--- src/states/threads/reducer.js
+++ src/states/threads/reducer.js
@@ -2,8 +2,8 @@ import { ActionType } from './action';
 
 function threadsReducer(threads = [], action = {}) {
   switch (action.type) {
-    case ActionType.RECEIVE_THREADS:
-      return action.payload.threads;
+    // case ActionType.RECEIVE_THREADS:
+    //   return action.payload.threads;
     case ActionType.ADD_THREAD:
       return [action.payload.thread, ...threads];
     default:
